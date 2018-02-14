d171a9b3d83f9e8b87346231bf2bf1844628a196
holymatch <holymatch@gmail.com>
2018-02-14 12:02:11 +0800

    Update PeopleController
    add update person

--- src/main/java/hk/hku/cs/faceweb/controller/PeopleController.java
+++ src/main/java/hk/hku/cs/faceweb/controller/PeopleController.java
@@ -61,9 +61,39 @@ public class PeopleController {
     public ResponseEntity<JsonResponseMessage<Person>> createPerson(@RequestBody Person person) {
         logger.debug("Person: " + person);
         ResponseEntity<JsonResponseMessage<Person>> response;
+        response = savePerson(person);
+        if (response.getStatusCode().equals(HttpStatus.OK)) {
+            response.getBody().setMessage("User create success");
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "/people/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<JsonResponseMessage<Person>> updatePerson(@PathVariable Long id, @RequestBody Person person) {
+
+        ResponseEntity<JsonResponseMessage<Person>> response;
+        logger.debug("Person: " + person);
+        if (id.equals(person.getId())) {
+            Person dbPerson = personService.findOne(id);
+            if (dbPerson == null ) {
+                response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(new JsonResponseMessage<Person>(person, HttpStatus.NOT_FOUND.value(), "Person with id " + id + " not found in db." ));
+            } else {
+                response = savePerson(person);
+                if (response.getStatusCode().equals(HttpStatus.OK)) {
+                    response.getBody().setMessage("User update success");
+                }
+            }
+        } else {
+            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new JsonResponseMessage<Person>(person, HttpStatus.BAD_REQUEST.value(), "The person id is not match with person object."));
+        }
+        return response;
+    }
+
+    private ResponseEntity<JsonResponseMessage<Person>> savePerson(Person person) {
+        ResponseEntity<JsonResponseMessage<Person>> response;
         try {
             person = personService.save(person);
-            response = ResponseEntity.ok(new JsonResponseMessage<Person>(person, HttpStatus.OK.value(), "User create success" ));
+            response = ResponseEntity.ok(new JsonResponseMessage<Person>(person, HttpStatus.OK.value(), null ));
         } catch (ResourceAccessException ex) {
             response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new JsonResponseMessage<Person>(null, HttpStatus.INTERNAL_SERVER_ERROR.value(), "Fail to connect face engine."));
             logger.error(ex.getMessage(), ex);
